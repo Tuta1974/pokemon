e9c56e26e2daae98ef4a8f533e394e6e41826524
VaneSanchez <nyagami@yahoo.com>
2023-05-19 13:22:25 -0300

    pull-info

--- src/components/PokemonGrid.js
+++ src/components/PokemonGrid.js
@@ -1,25 +1,58 @@
 import React, { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import PokemonCard from './PokemonCard';
+import ReactPaginate from 'react-paginate';
+import '../styles/react-paginate.css';
 
 function PokemonGrid() {
   const [pokemons, setPokemons] = useState([]);
+  const [limit] = useState(12);
+
+  const [currentPage, setCurrentPage] = useState(0);
+  const [itemsPerPage] = useState(20);
+
+  const manejarClick =(data) => {
+    setCurrentPage(data.selected);
+  }
+
+  const lastItem = (currentPage+ 1) * itemsPerPage;
+  const firstItem = lastItem - itemsPerPage;
+  const currentItem = pokemons.slice(firstItem, lastItem);
 
   useEffect(() => {
-    fetch('https://pokeapi.co/api/v2/pokemon')
+    fetch('https://pokeapi.co/api/v2/pokemon?limit=1008')
       .then(response => response.json())
-      .then(data => setPokemons(data.results));
-  }, []);
+      .then(data => {
+        setPokemons(data.results);
+        console.log(data.next);
+      
+      });
+  }, [limit]);
+
+
 
   return (
-    <Row className="g-4">
-      {pokemons.map(pokemon => (
-        <Col key={pokemon.name} xs={12} sm={6} md={3} xl={2}>
-          <PokemonCard pokemon={pokemon} />
-        </Col>
-      ))}
-    </Row>
+    <div>
+      <Row className="g-4">
+        {currentItem.map((pokemon) => (
+          <Col key={pokemon.name} xs={12} sm={6} md={3} xl={2}>
+            <PokemonCard pokemon={pokemon} />
+          </Col>
+        ))}
+      </Row>
+      <div className='py-3'>
+        <ReactPaginate
+        className='react-paginate'
+        pageCount={Math.ceil(pokemons.length / itemsPerPage)}
+        onPageChange={manejarClick}
+        previousLabel={"Anterior"}
+        nextLabel={"Siguiente"}
+      />
+      </div>
+      
+    </div>
   );
+  
 }
 
 export default PokemonGrid;
